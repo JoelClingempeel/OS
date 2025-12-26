f85ec373a13c8a420db2572326488ff583ab9760
Joel Clingempeel <joel.clingempeel@gmail.com>
2025-12-26 09:13:11 -0500

    Set up pic for hardware interrupts.

--- kernel.c
+++ kernel.c
@@ -40,14 +40,49 @@ struct IDTPointer init_idt() {
     return idt_ptr;
 }
 
+static inline uint8_t inb(uint16_t port) {
+    uint8_t ret;
+    asm volatile ( "inb %1, %0" : "=a"(ret) : "Nd"(port) );
+    return ret;
+}
+static inline void outb(uint16_t port, uint8_t val) {
+    asm volatile ( "outb %0, %1" : : "a"(val), "Nd"(port) );
+}
+
+void pic_remap(int offset1, int offset2) {
+    // Start initialization sequence (ICW1)
+    outb(0x20, 0x11); 
+    outb(0xA0, 0x11);
+
+    // Set Vector Offsets (ICW2)
+    outb(0x21, offset1); // Master offset (32)
+    outb(0xA1, offset2); // Slave offset (40)
+
+    // Tell Master there is a Slave at IRQ 2 (ICW3)
+    outb(0x21, 0x04);
+    // Tell Slave its identity is 2 (ICW3)
+    outb(0xA1, 0x02);
+
+    // Set 8086 mode (ICW4)
+    outb(0x21, 0x01);
+    outb(0xA1, 0x01);
+
+    // Mask everything to start
+    outb(0x21, 0xFF);
+    outb(0xA1, 0xFF);
+}
+
 void _kmain(void)
 {
     init_mem();
 
     struct IDTPointer idt_ptr = init_idt();
-
+    pic_remap(0x20, 0x28);
     asm volatile("lidt %0" : : "m" (idt_ptr));
-    asm volatile ("int $0x80"); 
+
+    asm volatile ("int $0x80");
+
+    outb(0xE9, 'V');  // Test
 
     while (1) {
         asm("hlt"); 
