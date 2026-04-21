dec8ba1c7c7014267a758be37c57f10037d3fa9b
Joel Clingempeel <joel.clingempeel@gmail.com>
2026-04-21 07:32:12 -0400

    Use char arrays to make previous bug fix less hacky.

--- src/user/lexer.c
+++ src/user/lexer.c
@@ -2,58 +2,38 @@
 
 static char tts_buf[20];
 
+static void buf_copy(char* dst, char* src) {
+    int i = 0;
+    while ((dst[i] = src[i])) i++;
+}
+
 const char* token_type_to_string(TokenType type) {
     switch (type) {
-        case TOKEN_IF:
-            tts_buf[0]='I';tts_buf[1]='F';tts_buf[2]=0; break;
-        case TOKEN_ELSE:
-            tts_buf[0]='E';tts_buf[1]='L';tts_buf[2]='S';tts_buf[3]='E';tts_buf[4]=0; break;
-        case TOKEN_WHILE:
-            tts_buf[0]='W';tts_buf[1]='H';tts_buf[2]='I';tts_buf[3]='L';tts_buf[4]='E';tts_buf[5]=0; break;
-        case TOKEN_IDENTIFIER:
-            tts_buf[0]='I';tts_buf[1]='D';tts_buf[2]='E';tts_buf[3]='N';tts_buf[4]='T';tts_buf[5]=0; break;
-        case TOKEN_NUMBER:
-            tts_buf[0]='N';tts_buf[1]='U';tts_buf[2]='M';tts_buf[3]=0; break;
-        case TOKEN_ADD:
-            tts_buf[0]='A';tts_buf[1]='D';tts_buf[2]='D';tts_buf[3]=0; break;
-        case TOKEN_SUBTRACT:
-            tts_buf[0]='S';tts_buf[1]='U';tts_buf[2]='B';tts_buf[3]=0; break;
-        case TOKEN_MULTIPLY:
-            tts_buf[0]='M';tts_buf[1]='U';tts_buf[2]='L';tts_buf[3]=0; break;
-        case TOKEN_DIVIDE:
-            tts_buf[0]='D';tts_buf[1]='I';tts_buf[2]='V';tts_buf[3]=0; break;
-        case TOKEN_SEMICOLON:
-            tts_buf[0]='S';tts_buf[1]='E';tts_buf[2]='M';tts_buf[3]='I';tts_buf[4]=0; break;
-        case TOKEN_LEFT_PAREN:
-            tts_buf[0]='L';tts_buf[1]='P';tts_buf[2]='A';tts_buf[3]='R';tts_buf[4]='N';tts_buf[5]=0; break;
-        case TOKEN_RIGHT_PAREN:
-            tts_buf[0]='R';tts_buf[1]='P';tts_buf[2]='A';tts_buf[3]='R';tts_buf[4]='N';tts_buf[5]=0; break;
-        case TOKEN_LEFT_BRACE:
-            tts_buf[0]='L';tts_buf[1]='B';tts_buf[2]='R';tts_buf[3]='C';tts_buf[4]='E';tts_buf[5]=0; break;
-        case TOKEN_RIGHT_BRACE:
-            tts_buf[0]='R';tts_buf[1]='B';tts_buf[2]='R';tts_buf[3]='C';tts_buf[4]='E';tts_buf[5]=0; break;
-        case TOKEN_EQUALS:
-            tts_buf[0]='E';tts_buf[1]='Q';tts_buf[2]=0; break;
-        case TOKEN_LESS:
-            tts_buf[0]='L';tts_buf[1]='T';tts_buf[2]=0; break;
-        case TOKEN_LESS_EQUALS:
-            tts_buf[0]='L';tts_buf[1]='E';tts_buf[2]=0; break;
-        case TOKEN_GREATER:
-            tts_buf[0]='G';tts_buf[1]='T';tts_buf[2]=0; break;
-        case TOKEN_GREATER_EQUALS:
-            tts_buf[0]='G';tts_buf[1]='E';tts_buf[2]=0; break;
-        case TOKEN_DOUBLE_EQUALS:
-            tts_buf[0]='E';tts_buf[1]='Q';tts_buf[2]='Q';tts_buf[3]=0; break;
-        case TOKEN_NOT_EQUALS:
-            tts_buf[0]='N';tts_buf[1]='E';tts_buf[2]=0; break;
-        case TOKEN_FUN:
-            tts_buf[0]='F';tts_buf[1]='U';tts_buf[2]='N';tts_buf[3]=0; break;
-        case TOKEN_COMMA:
-            tts_buf[0]='C';tts_buf[1]='M';tts_buf[2]='A';tts_buf[3]=0; break;
-        case TOKEN_RETURN:
-            tts_buf[0]='R';tts_buf[1]='E';tts_buf[2]='T';tts_buf[3]=0; break;
-        default:
-            tts_buf[0]='?';tts_buf[1]=0; break;
+        case TOKEN_IF:             { char s[] = "IF";           buf_copy(tts_buf, s); break; }
+        case TOKEN_ELSE:           { char s[] = "ELSE";         buf_copy(tts_buf, s); break; }
+        case TOKEN_WHILE:          { char s[] = "WHILE";        buf_copy(tts_buf, s); break; }
+        case TOKEN_IDENTIFIER:     { char s[] = "IDENT";        buf_copy(tts_buf, s); break; }
+        case TOKEN_NUMBER:         { char s[] = "NUM";          buf_copy(tts_buf, s); break; }
+        case TOKEN_ADD:            { char s[] = "ADD";          buf_copy(tts_buf, s); break; }
+        case TOKEN_SUBTRACT:       { char s[] = "SUB";          buf_copy(tts_buf, s); break; }
+        case TOKEN_MULTIPLY:       { char s[] = "MUL";          buf_copy(tts_buf, s); break; }
+        case TOKEN_DIVIDE:         { char s[] = "DIV";          buf_copy(tts_buf, s); break; }
+        case TOKEN_SEMICOLON:      { char s[] = "SEMI";         buf_copy(tts_buf, s); break; }
+        case TOKEN_LEFT_PAREN:     { char s[] = "LPARN";        buf_copy(tts_buf, s); break; }
+        case TOKEN_RIGHT_PAREN:    { char s[] = "RPARN";        buf_copy(tts_buf, s); break; }
+        case TOKEN_LEFT_BRACE:     { char s[] = "LBRCE";        buf_copy(tts_buf, s); break; }
+        case TOKEN_RIGHT_BRACE:    { char s[] = "RBRCE";        buf_copy(tts_buf, s); break; }
+        case TOKEN_EQUALS:         { char s[] = "EQ";           buf_copy(tts_buf, s); break; }
+        case TOKEN_LESS:           { char s[] = "LT";           buf_copy(tts_buf, s); break; }
+        case TOKEN_LESS_EQUALS:    { char s[] = "LE";           buf_copy(tts_buf, s); break; }
+        case TOKEN_GREATER:        { char s[] = "GT";           buf_copy(tts_buf, s); break; }
+        case TOKEN_GREATER_EQUALS: { char s[] = "GE";           buf_copy(tts_buf, s); break; }
+        case TOKEN_DOUBLE_EQUALS:  { char s[] = "EQQ";          buf_copy(tts_buf, s); break; }
+        case TOKEN_NOT_EQUALS:     { char s[] = "NE";           buf_copy(tts_buf, s); break; }
+        case TOKEN_FUN:            { char s[] = "FUN";          buf_copy(tts_buf, s); break; }
+        case TOKEN_COMMA:          { char s[] = "CMA";          buf_copy(tts_buf, s); break; }
+        case TOKEN_RETURN:         { char s[] = "RET";          buf_copy(tts_buf, s); break; }
+        default:                   { tts_buf[0] = '?'; tts_buf[1] = 0;               break; }
     }
     return tts_buf;
 }
@@ -66,22 +46,23 @@ static int is_digit(char c) {
     return c >= '0' && c <= '9';
 }
 
-// All comparisons use character literals (embedded in .text), not string
-// literals (which would land in .rodata and read as zero due to linker layout).
+static int sv_equals(StringView sv, const char* str) {
+    int i = 0;
+    while (i < sv.len && str[i] && sv.ptr[i] == str[i]) i++;
+    return i == sv.len && str[i] == '\0';
+}
+
 static TokenType check_keyword(StringView sv) {
-    if (sv.len == 2 && sv.ptr[0] == 'i' && sv.ptr[1] == 'f')
-        return TOKEN_IF;
-    if (sv.len == 4 && sv.ptr[0] == 'e' && sv.ptr[1] == 'l' &&
-        sv.ptr[2] == 's' && sv.ptr[3] == 'e')
-        return TOKEN_ELSE;
-    if (sv.len == 5 && sv.ptr[0] == 'w' && sv.ptr[1] == 'h' &&
-        sv.ptr[2] == 'i' && sv.ptr[3] == 'l' && sv.ptr[4] == 'e')
-        return TOKEN_WHILE;
-    if (sv.len == 3 && sv.ptr[0] == 'f' && sv.ptr[1] == 'u' && sv.ptr[2] == 'n')
-        return TOKEN_FUN;
-    if (sv.len == 6 && sv.ptr[0] == 'r' && sv.ptr[1] == 'e' &&
-        sv.ptr[2] == 't' && sv.ptr[3] == 'u' && sv.ptr[4] == 'r' && sv.ptr[5] == 'n')
-        return TOKEN_RETURN;
+    char kw_if[]     = "if";
+    char kw_else[]   = "else";
+    char kw_while[]  = "while";
+    char kw_fun[]    = "fun";
+    char kw_return[] = "return";
+    if (sv_equals(sv, kw_if))     return TOKEN_IF;
+    if (sv_equals(sv, kw_else))   return TOKEN_ELSE;
+    if (sv_equals(sv, kw_while))  return TOKEN_WHILE;
+    if (sv_equals(sv, kw_fun))    return TOKEN_FUN;
+    if (sv_equals(sv, kw_return)) return TOKEN_RETURN;
     return TOKEN_IDENTIFIER;
 }
 
