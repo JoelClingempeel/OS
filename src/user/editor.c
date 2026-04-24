9d1da489d6914a42a3f09d4c3ff4169e31a3ef18
Joel Clingempeel <joel.clingempeel@gmail.com>
2026-04-23 21:14:41 -0400

    Enabling scrolling up and down in editor by entering commands u and d.

--- src/user/editor.c
+++ src/user/editor.c
@@ -2,23 +2,31 @@
 #include "user_progs.h"
 #include "utils.h"
 
-#define EDITOR_LINES 24
-// Max content chars per line: 80 columns minus the 4-char "os% " prompt.
-#define LINE_WIDTH 76
+#define EDITOR_LINES 48
+#define VIEW_LINES   24
+#define LINE_WIDTH   76
+#define SCROLL_STEP   4
 
-static void display_line(int idx, char lines[][80]) {
+static void display_line(int screen_row, int actual_idx, char lines[][80]) {
     char display[80];
-    int num = idx + 1;
+    int num = actual_idx + 1;
     int pos = 0;
     if (num < 10) display[pos++] = ' ';
     char num_str[4];
     uint_to_ascii((uint32_t)num, num_str);
     for (int i = 0; num_str[i]; i++) display[pos++] = num_str[i];
     display[pos++] = ' ';
-    for (int j = 0; lines[idx][j] && pos < 79; j++)
-        display[pos++] = lines[idx][j];
+    for (int j = 0; lines[actual_idx][j] && pos < 79; j++)
+        display[pos++] = lines[actual_idx][j];
     display[pos] = '\0';
-    user_print_line(display, idx);
+    user_print_line(display, screen_row);
+}
+
+static void redisplay(int scroll_offset, char lines[][80]) {
+    for (int i = 0; i < VIEW_LINES; i++) {
+        user_clear_line(i);
+        display_line(i, scroll_offset + i, lines);
+    }
 }
 
 static void parse_into_lines(char* buf, char lines[][80]) {
@@ -33,9 +41,8 @@ static void parse_into_lines(char* buf, char lines[][80]) {
             lines[line_idx][col] = '\0';
             line_idx++;
             col = 0;
-            if (line_idx < EDITOR_LINES) {
+            if (line_idx < EDITOR_LINES)
                 lines[line_idx][col++] = buf[i];
-            }
         } else {
             lines[line_idx][col++] = buf[i];
         }
@@ -71,13 +78,11 @@ void editor() {
     if (path && path[0] && fs_read(path, file_buf) == 0)
         parse_into_lines(file_buf, lines);
 
-    for (int i = 0; i < EDITOR_LINES; i++) {
-        user_clear_line(i);
-        display_line(i, lines);
-    }
+    int scroll_offset = 0;
+    redisplay(scroll_offset, lines);
 
     while (1) {
-        char* input = get(24);
+        char* input = get(VIEW_LINES);
 
         if (input[0] == 'e' && input[1] == '\0') break;
 
@@ -87,23 +92,44 @@ void editor() {
             continue;
         }
 
+        if (input[0] == 'u' && input[1] == '\0') {
+            if (scroll_offset > 0) {
+                scroll_offset -= SCROLL_STEP;
+                if (scroll_offset < 0) scroll_offset = 0;
+                redisplay(scroll_offset, lines);
+            }
+            continue;
+        }
+
+        if (input[0] == 'd' && input[1] == '\0') {
+            if (scroll_offset < EDITOR_LINES - VIEW_LINES) {
+                scroll_offset += SCROLL_STEP;
+                if (scroll_offset > EDITOR_LINES - VIEW_LINES)
+                    scroll_offset = EDITOR_LINES - VIEW_LINES;
+                redisplay(scroll_offset, lines);
+            }
+            continue;
+        }
+
         uint32_t selected = str_to_uint(input);
-        if (selected < 1 || selected > EDITOR_LINES) continue;
+        if (selected < (uint32_t)(scroll_offset + 1) ||
+            selected > (uint32_t)(scroll_offset + VIEW_LINES)) continue;
 
-        int sel_idx = (int)(selected - 1);
+        int actual_idx  = (int)(selected - 1);
+        int screen_row  = actual_idx - scroll_offset;
 
-        user_clear_line(sel_idx);
-        char* new_content = get_prefilled(sel_idx, lines[sel_idx]);
+        user_clear_line(screen_row);
+        char* new_content = get_prefilled(screen_row, lines[actual_idx]);
 
         int j = 0;
         while (new_content[j] && j < LINE_WIDTH) {
-            lines[sel_idx][j] = new_content[j];
+            lines[actual_idx][j] = new_content[j];
             j++;
         }
-        lines[sel_idx][j] = '\0';
+        lines[actual_idx][j] = '\0';
 
-        user_clear_line(sel_idx);
-        display_line(sel_idx, lines);
+        user_clear_line(screen_row);
+        display_line(screen_row, actual_idx, lines);
     }
 
     user_clear_terminal();
